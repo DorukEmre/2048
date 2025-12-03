881867c79fa730007ec6dd6802c3674a1fa37b3d
Doruk Emre <doruk.emre@outlook.com>
2025-12-03 10:13:15 +0100

    Implement working gameover check

--- frontend/js/gameover.js
+++ frontend/js/gameover.js
@@ -2,13 +2,12 @@ import { gameState as gs } from './gameData.js';
 
 function testMoveUp() {
   for (let i = 0; i <= 15; i++) {
-    if (gs.squares[i].canMove && gs.squares[i].y > 0) {
-      for (let j = gs.squares[i].y - 1; j >= 0; j--) {
-        let checkIndex = gs.squares[i].x + j * 4;
-        if (gs.squares[checkIndex].canMove == false || gs.squares[checkIndex].value == gs.squares[i].value) {
-          return true;
-        }
-      }
+    if (gs.squares[i].canMove
+      && gs.squares[i].y > 0
+      && (gs.squares[i - 4].canMove == false
+        || gs.squares[i - 4].value == gs.squares[i].value)
+    ) {
+      return true;
     }
   }
   return false;
@@ -16,13 +15,12 @@ function testMoveUp() {
 
 function testMoveDown() {
   for (let i = 15; i >= 0; i--) {
-    if (gs.squares[i].canMove && gs.squares[i].y < 3) {
-      for (let j = gs.squares[i].y + 1; j <= 3; j++) {
-        let checkIndex = gs.squares[i].x + j * 4;
-        if (gs.squares[checkIndex].canMove == false || gs.squares[checkIndex].value == gs.squares[i].value) {
-          return true;
-        }
-      }
+    if (gs.squares[i].canMove
+      && gs.squares[i].y < 3
+      && (gs.squares[i + 4].canMove == false
+        || gs.squares[i + 4].value == gs.squares[i].value)
+    ) {
+      return true;
     }
   }
   return false;
@@ -30,13 +28,12 @@ function testMoveDown() {
 
 function testMoveLeft() {
   for (let i = 0; i <= 15; i++) {
-    if (gs.squares[i].canMove && gs.squares[i].x > 0) {
-      for (let j = gs.squares[i].x - 1; j >= 0; j--) {
-        let checkIndex = j + gs.squares[i].y * 4;
-        if (gs.squares[checkIndex].canMove == false || gs.squares[checkIndex].value == gs.squares[i].value) {
-          return true;
-        }
-      }
+    if (gs.squares[i].canMove
+      && gs.squares[i].x > 0
+      && (gs.squares[i - 1].canMove == false
+        || gs.squares[i - 1].value == gs.squares[i].value)
+    ) {
+      return true;
     }
   }
   return false;
@@ -44,20 +41,18 @@ function testMoveLeft() {
 
 function testMoveRight() {
   for (let i = 15; i >= 0; i--) {
-    if (gs.squares[i].canMove && gs.squares[i].x < 3) {
-      for (let j = gs.squares[i].x + 1; j <= 3; j++) {
-        let checkIndex = j + gs.squares[i].y * 4;
-        if (gs.squares[checkIndex].canMove == false || gs.squares[checkIndex].value == gs.squares[i].value) {
-          return true;
-        }
-      }
+    if (gs.squares[i].canMove
+      && gs.squares[i].x < 3
+      && (gs.squares[i + 1].canMove == false
+        || gs.squares[i + 1].value == gs.squares[i].value)
+    ) {
+      return true;
     }
   }
   return false;
 }
 
 function checkGameOver() {
-  return false;
 
   let canMoveUp = testMoveUp();
   let canMoveDown = testMoveDown();
